c624313ee4e0d0cca88a7484789016ea67e5079b
JuntaoLi79 <61711097+shane1595042264@users.noreply.github.com>
2023-05-12 11:12:13 -0400

    Create the app

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        {/* Add your navigation menu items here */}
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/services">Services</a>
+        <a href="/contact">Contact</a>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
